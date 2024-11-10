affa3b4da66f4dad6df8b93e75f4c7b2fcd85080
Flix <flixgit@outlook.com>
2024-11-09 20:27:28 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,52 +6,41 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-let players = {};
-let food = [];
+let players = [];
 
-// Serve static files (for the frontend)
 app.use(express.static('public'));
 
-// Create random food on the map
-function createFood() {
-    const x = Math.random() * 800;
-    const y = Math.random() * 600;
-    food.push({ x, y });
-}
-
-createFood(); // Initial food spawn
-
-// Handle new player connection
 io.on('connection', (socket) => {
     console.log('A player connected: ' + socket.id);
 
-    // Handle new player joining
-    socket.on('newPlayer', (nickname) => {
-        players[socket.id] = { x: 100, y: 100, size: 10, body: [{ x: 100, y: 100 }], id: socket.id, color: 'green', nickname };
+    socket.on('newPlayer', (data) => {
+        const newPlayer = {
+            id: socket.id,
+            nickname: data.nickname,
+            color: data.snakeColor,
+            x: Math.random() * 500,
+            y: Math.random() * 500,
+            size: 10,
+            body: [{ x: Math.random() * 500, y: Math.random() * 500 }],
+        };
+        players.push(newPlayer);
+        io.emit('updatePlayer', newPlayer);
     });
 
-    // Handle player movement
-    socket.on('move', (data) => {
-        players[socket.id].x = data.x;
-        players[socket.id].y = data.y;
-        players[socket.id].body = data.body;
-        players[socket.id].size = data.size;
-        players[socket.id].color = data.color;
-    });
-
-    // Send game state to all clients every frame
-    setInterval(() => {
-        io.emit('gameState', players);
-    }, 1000 / 60); // 60 FPS
-
-    // Handle player disconnect
     socket.on('disconnect', () => {
-        console.log('Player disconnected: ' + socket.id);
-        delete players[socket.id];
+        players = players.filter(player => player.id !== socket.id);
+        io.emit('updateLeaderboard', players);
+    });
+
+    socket.on('gameOver', () => {
+        io.emit('gameOver');
     });
 });
 
-// Start server
+setInterval(() => {
+    io.emit('updateLeaderboard', players);
+}, 5000);
+
 server.listen(3000, () => {
-    console.log('Server started on http://localhost:3000');
+    console.log('Server running on port 3000');
 });
